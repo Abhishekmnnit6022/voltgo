d627982aea8f5487ca4b7d6eb12248e9a598e31e
Abhishekmnnit6022 <abhishek.20238030@mnnit.ac.in>
2024-06-27 13:55:22 +0530

    Loaded fonts

--- App.js
+++ App.js
@@ -1,7 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen'
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const[fontloaded]=useFonts({
+    'Semibold':require('./assets/fonts/Outfit-SemiBold.ttf'),
+    'Extrabold':require('./assets/fonts/Outfit-ExtraBold.ttf'),
+    'Extralight':require('./assets/fonts/Outfit-ExtraLight.ttf'),
+     'Medium':require('./assets/fonts/Outfit-Medium.ttf'),
+     'Regular':require('./assets/fonts/Outfit-Regular.ttf'),
+     'Thin':require('./assets/fonts/Outfit-Thin.ttf'),
+  });
+
+  const onLayoutrootView=useCallback(async()=>{
+    if(fontloaded){
+      await SplashScreen.hideAsync();
+    }
+    },[fontloaded]);
+
+    if(!fontloaded){
+      return null;
+    }  
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
