ebbd1c85e946ba1a9f71d21b3531f1c5a37dbbcc
Abhishekmnnit6022 <abhishek.20238030@mnnit.ac.in>
2024-07-01 00:04:57 +0530

    User current location

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import { useCallback } from 'react';
-import LoginScreen from './App/screens/Login/LoginScreen';
+import * as Location from 'expo-location';
 import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
 import * as SecureStore from 'expo-secure-store';
 import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from './App/screens/Login/LoginScreen';
 import TabNavigation from './App/Navigation/TabNavigation';
+import  UserLocationContext  from './App/context/UserLocationcontext';
 
 SplashScreen.preventAutoHideAsync();
+
 const tokenCache = {
   getToken(key) {
     try {
@@ -29,13 +31,38 @@ const tokenCache = {
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    'SemiBold': require('./assets/fonts/Outfit-SemiBold.ttf'),
-    'ExtraBold': require('./assets/fonts/Outfit-ExtraBold.ttf'),
-    'ExtraLight': require('./assets/fonts/Outfit-ExtraLight.ttf'),
-    'Medium': require('./assets/fonts/Outfit-Medium.ttf'),
-    'Regular': require('./assets/fonts/Outfit-Regular.ttf'),
-    'Thin': require('./assets/fonts/Outfit-Thin.ttf'),
+    SemiBold: require('./assets/fonts/Outfit-SemiBold.ttf'),
+    ExtraBold: require('./assets/fonts/Outfit-ExtraBold.ttf'),
+    ExtraLight: require('./assets/fonts/Outfit-ExtraLight.ttf'),
+    Medium: require('./assets/fonts/Outfit-Medium.ttf'),
+    Regular: require('./assets/fonts/Outfit-Regular.ttf'),
+    Thin: require('./assets/fonts/Outfit-Thin.ttf'),
   });
+
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location.coords);
+      console.log(location.coords);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
 
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
@@ -47,20 +74,24 @@ export default function App() {
     return null;
   }
 
+
+
   return (
-    <ClerkProvider 
+    <ClerkProvider
       tokenCache={tokenCache}
-      publishableKey={'pk_test_cHJlc2VudC1sZW9wYXJkLTgyLmNsZXJrLmFjY291bnRzLmRldiQ'}
+      publishableKey="pk_test_cHJlc2VudC1sZW9wYXJkLTgyLmNsZXJrLmFjY291bnRzLmRldiQ"
     >
       <NavigationContainer>
-        <View style={styles.container} onLayout={onLayoutRootView}>
-          <SignedIn>
-            <TabNavigation />
-          </SignedIn>
-          <SignedOut>
-            <LoginScreen />
-          </SignedOut>
-        </View>
+        <UserLocationContext.Provider value={{ location, setLocation }}>
+          <View style={styles.container} onLayout={onLayoutRootView}>
+            <SignedIn>
+              <TabNavigation />
+            </SignedIn>
+            <SignedOut>
+              <LoginScreen />
+            </SignedOut>
+          </View>
+        </UserLocationContext.Provider>
       </NavigationContainer>
     </ClerkProvider>
   );
--- App/screens/Homesceen/Mapview.jsx
+++ App/screens/Homesceen/Mapview.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import React, { useContext } from 'react';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 import mapStyle from './../../../constants/mapstyle.json';
+import UserLocationContext from './../../context/UserLocationcontext';
 
 export default function AppMapview() {
-return (
+  const { location } = useContext(UserLocationContext);
+
+  if (!location?.latitude) {
+    return null;
+  }
+
+  return (
     <View style={styles.container}>
-        <MapView
-            style={styles.map}   
-            region={{
-                latitude: 20.5937,
-                longitude: 78.9629,
-                latitudeDelta: 12,
-                longitudeDelta: 12,
-            }}
-            showsUserLocation={true} 
-            customMapStyle={mapStyle}              
-        />        
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        region={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+        }}
+        customMapStyle={mapStyle}
+        userLocationAnnotationTitle="Your Location"
+      >
+        <Marker
+          coordinate={{ latitude: location.latitude, longitude: location.longitude }}
+          title="Your Location"
+        />
+      </MapView>
     </View>
-);
+  );
 }
 
 const styles = StyleSheet.create({
